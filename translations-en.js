f986681ec7a4bb419bb050f6246ca0297a4818ef
Mateo Parra <mparra@yuxiglobal.com>
2017-08-14 13:42:14 -0500

    cambios imagenes 2

--- translations-en.js
+++ translations-en.js
@@ -40,12 +40,12 @@ var JSON_TRANSLATIONS_EN_OBJECT =
     If the number of Satellites selected is an unacceptable configuration, a symbol <i class="fa fa-times-circle check-icon" aria-hidden="true"></i> will appear in one of the five verification areas. <br> <br>
     When this symbol <i class="fa fa-times-circle check-icon" aria-hidden="true"></i> appears, you must change the selections. <br> <br>
     A successful configuration will be indicated as shown below.<br>
-    <img class="imgInstructions" src="rsc/successfull.png">`,
+    <img class="imgInstructions" src="rsc/success.png">`,
     "RLE-DES04": `Select the Satellites to be placed on the left side of the Main Cabinet. <br> <br>
     If the number of Satellites selected is an unacceptable configuration, a symbol <i class="fa fa-times-circle check-icon" aria-hidden="true"></i> will appear in one of the five verification areas. <br> <br>
     When this symbol <i class="fa fa-times-circle check-icon" aria-hidden="true"></i> appears, you must change the selections. <br> <br>
     A successful configuration will be indicated as shown below.<br>
-    <img class="imgInstructions" src="rsc/successfull.png">`,
+    <img class="imgInstructions" src="rsc/success.png">`,
     "RLE-DES05": `If the selections violate any of the five verifications, this will message will appear <br> <br>
     <img class="imgInstructions" src="rsc/unsuccess.png"><br> <br>
     When the above message appears, you must change the selections until the successful message appears <br> <br>
